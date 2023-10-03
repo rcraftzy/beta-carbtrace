23d1ed53822aa18388d30ca9ff43a2152b708da2
Speedev <roberto.tg99@gmail.com>
2023-10-03 14:55:12 -0500

    feat environ for database

--- backend/__init__.py
+++ backend/__init__.py
@@ -11,10 +11,10 @@ def create_app():
     app.secret_key = os.urandom(24)
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')  
 
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'smoke'
-    app.config['MYSQL_DB'] = 'mydb'
+    app.config['MYSQL_HOST'] = os.environ.get('DATABASE_HOST')
+    app.config['MYSQL_USER'] = os.environ.get('DATABASE_USER')
+    app.config['MYSQL_PASSWORD'] = os.environ.get('DATABASE_PASSWORD')
+    app.config['MYSQL_DB'] = os.environ.get('DATABASE')
 
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587  # Puerto del servidor de correo
